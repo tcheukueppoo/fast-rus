56ebd926ca10624801f12c01081105a6f4c4229f
tcheukueppo <tcheukueppo@yandex.com>
2025-04-12 20:48:57 +0100

    Fix issue in the `->synthesize()` method

--- lib/Fast/Rus/Synthesizer.pm
+++ lib/Fast/Rus/Synthesizer.pm
@@ -1,34 +1,62 @@
 use strict;
 use warnings;
 
-use feature 'class';
 use v5.38;
+use feature 'class';
 
-use Carp;
-use Mojo::UserAgent;
+no warnings 'experimental';
 
-class Synthesizer {
-   my $ua = Mojo::UserAgent->new;
-   my $url = Mojo::URL->scheme('https')
-   ->host('www.google.com')
-   ->path('async/translatte_tts')
-   ->query(ei => 'emXtZ4rGMaymhbIPsY3nmQ8')
-   ->query(opi => 89978449)
-   ->query(sca_esv => 'c4fe9279b750d491')
-   ->query(cs => 1)
-   ->query(async => '_fmt:jspb')
-   ->query(yv => 3);
+class Fast::Rus::Synthesizer {
 
-   field $speed = 1;
-   field $text  = 'Hello World!';
+  use Carp qw(croak);
+  use Mojo::UserAgent;
+  use Mojo::URL;
+  use Mojo::Util qw(url_escape);
+  use Mojo::JSON qw(decode_json);
+  use Encode;
 
-   method synthesize () {
-      my $enc_text = 
-      my $syn_url  = $url->query(tts => "txt:$enc_text,spd:$speed");
-      my $tx = $ua->get("$syn_url");
+  my $ua  = Mojo::UserAgent->new;
+  my $url = Mojo::URL->new->scheme('https')
+    ->host('www.google.com')
+    ->path('async/translate_tts')
+    ->query(
+            ei      => 'emXtZ4rGMaymhbIPsY3nmQ8',
+            opi     => 89978449,
+            sca_esv => 'c4fe9279b750d491',
+            cs      => 1,
+            async   => '_fmt:jspb',
+            yv      => 3,
+           );
 
-      croak "Connection error: " . $tx->message if $tx->is_error;
+  field $speed = 2;
+  field $text //= 'Привет, мир!';
 
-   }
+  method text ($new_text) {
+    $text = $new_text;
+    return $self;
+  }
 
+  method synthesize () {
+    my $enc_text = url_escape($text =~ s/(.)/sprintf '%%%02X', ord $1/gre, '%');
+    my $syn_url  = $url . "&ttsp=tl:ru,txt:$enc_text,spd:$speed";
+    my $tx       = $ua->get($syn_url);
+
+    croak("Connection error: " . $tx->result->message) if !$tx->result->is_success;
+    say decode_json(substr $tx->result->text, 5)->{'translate_tts'}[0];
+  }
+
+  method speedup () {
+    $speed = 1.23;
+    $self->synthesize();
+  }
+
+  method slowdown () {
+    $speed-- if $speed > 1;
+    $self->synthesize();
+  }
+
+  method reset_speed () {
+    $speed = 1;
+    return $self;
+  }
 }
